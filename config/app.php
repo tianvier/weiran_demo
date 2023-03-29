a55aef5ae8a7bf21b43795d94c511338f02539f7
duoli <zhaody901@126.com>
2023-03-30 06:48:30 +0800

    fix: form and html not auto load

--- config/app.php
+++ config/app.php
@@ -207,5 +207,8 @@ return [
         'Url'          => Illuminate\Support\Facades\URL::class,
         'Validator'    => Illuminate\Support\Facades\Validator::class,
         'View'         => Illuminate\Support\Facades\View::class,
+
+        'Form' => Poppy\MgrPage\Facade\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
     ],
 ];
